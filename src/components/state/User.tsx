0692cb1ee40c0ada19a074b2b299fc8f6e90ba44
Emily LeBlanc <emilyleblanc@Emilys-MBP.hitronhub.home>
2022-10-19 13:34:14 -0400

    added Counter.tsx

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 type AuthUser = {
     name: string
@@ -6,8 +6,12 @@ type AuthUser = {
 }
 
 export function User() {
-    //"useState<AuthUser | null>(null)" this inferes types so you can have the initial state set to null prior to a value being set. Now the inital value is null and the AuthUser values can be used in the setUser function.
+    //"useState<AuthUser | null>(null)" this infers types so you can have the initial state set to null prior to a value being set. Now the initial value is null and the AuthUser values can be used in the setUser function.
     const [user, setUser] = useState<AuthUser | null>(null)
+
+    //below is a "type assertion" example. The best use case for this example is if the state is changed from null shortly after the component renders and will not change. In this case it would be if the user could not log out. 
+    //eg.
+    //const [user, setUser] = useState<AuthUser>({} as AuthUser)
 
     const handleLogin = () => { setUser({ name:'Emily', email:'elb@funtimes.com'}) }
     const handleLogout = () => { setUser(null) } 
